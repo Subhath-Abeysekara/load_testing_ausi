bcfb4f02074952a6221e245d2858f512706f7d16
jerom <jeromroshane911@gmail.com>
2024-09-13 08:19:17 +0530

    video url

--- addVideo.ts
+++ addVideo.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { authenticate } from "./authentication/authenticate_token";
 import {run_video} from "./connection_mongo";
 import { auth_return, user, video } from "./types";
@@ -12,9 +13,11 @@ const operation = async (req:any , res:any) => {
         }
         else{
         const client = await run_video()
+        console.log("name",req.body.name)
+        const extension = path.extname(req.file.originalname)
         const body:video= {
                 user_id:validity.userId,
-                url : req.body.name,
+                url : `${req.body.name}${extension}`,
                 title:req.body.title
         }
         const data = await client.insertOne(body);

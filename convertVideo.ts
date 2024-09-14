d39444f8c0359d0bd9e8d7effce720d6b2507b8e
jerom <jeromroshane911@gmail.com>
2024-09-14 07:27:54 +0530

    check upload

--- convertVideo.ts
+++ convertVideo.ts
@@ -14,18 +14,18 @@ const operation = async (req:any , res:any) => {
             return
         }
         else{
-        let client = await run_download()
+        let client = await run_video()
+        const video = await client.findOne({_id : new ObjectId(req.body.video_id)});
+        console.log('video',video)
+        client = await run_download()
         const body:download= {
                 user_id:validity.userId,
-                video_id : req.body.video_id,
+                video_id : video?.title,
                 time_stamp: Date.now().toString(),
                 download_type:req.body.type
         }
         const data = await client.insertOne(body);
-        client = await run_video()
         console.log(data);
-        const video = await client.findOne({_id : new ObjectId(req.body.video_id)});
-        console.log('video',video)
         const postData: axio_post = {
             url: video?.url,
             type:req.body.type
@@ -41,65 +41,11 @@ const operation = async (req:any , res:any) => {
                   responseType: 'stream', 
               }
           );
-          function isPrime(num:any) {
-            if (num <= 1) return false;
-            if (num <= 3) return true;
-            
-            if (num % 2 === 0 || num % 3 === 0) return false;
-            
-            for (let i = 5; i * i <= num; i += 6) {
-              if (num % i === 0 || num % (i + 2) === 0) return false;
-            }
-            
-            return true;
-          }
-
-          function loadCPU(limit:any) {
-            let primeCount = 0;
-            
-            for (let i = 2; i < limit; i++) {
-              if (isPrime(i)) {
-                primeCount++;
-              }
-            }
-            
-            console.log(`Found ${primeCount} prime numbers up to ${limit}.`);
-          }
-          const limit = 100000;
-            console.log("Starting CPU loading operation...");
-            loadCPU(limit);
           res.setHeader('Content-Type', `video/${req.body.type}`); 
           res.setHeader('Content-Disposition', 'inline'); 
           axiosResponse.data.pipe(res);
       
       } catch (error) {
-        function isPrime(num:any) {
-            if (num <= 1) return false;
-            if (num <= 3) return true;
-            
-            if (num % 2 === 0 || num % 3 === 0) return false;
-            
-            for (let i = 5; i * i <= num; i += 6) {
-              if (num % i === 0 || num % (i + 2) === 0) return false;
-            }
-            
-            return true;
-          }
-
-          function loadCPU(limit:any) {
-            let primeCount = 0;
-            
-            for (let i = 2; i < limit; i++) {
-              if (isPrime(i)) {
-                primeCount++;
-              }
-            }
-            
-            console.log(`Found ${primeCount} prime numbers up to ${limit}.`);
-          }
-          const limit = 100000;
-            console.log("Starting CPU loading operation...");
-            loadCPU(limit);
           console.error('Error:', error);
           res.status(500).send('Error occurred while sending video');
       }
